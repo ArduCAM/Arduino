d3a45de1dbf9a97558b81d8d5f9a8e4eac5b1a77
arducam <admin@arducam.com>
2016-08-06 22:51:52 +0800

    Update host application

---
+++ ArduCAM/examples/host_app/ArduCAM_Mini_V2.0_Linux_x86_64bit/run.sh
@@ -0,0 +1,14 @@
+ln -s $PWD/libQt5Core.so.5.7.0 libQt5Core.so.5
+ln -s $PWD/libQt5Widgets.so.5.7.0 libQt5Widgets.so.5
+ln -s $PWD/libQt5Gui.so.5.7.0 libQt5Gui.so.5
+ln -s $PWD/libQt5Network.so.5.7.0 libQt5Network.so.5
+ln -s $PWD/libQt5SerialPort.so.5.7.0 libQt5SerialPort.so.5
+ln -s $PWD/libicudata.so.56.1 libicudata.so.56
+ln -s $PWD/libicui18n.so.56.1 libicui18n.so.56
+ln -s $PWD/libicuuc.so.56.1 libicuuc.so.56
+ln -s $PWD/libQt5XcbQpa.so.5.7.0 libQt5XcbQpa.so.5
+ln -s $PWD/libQt5DBus.so.5.7.0 libQt5DBus.so.5
+
+
+export set LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+
