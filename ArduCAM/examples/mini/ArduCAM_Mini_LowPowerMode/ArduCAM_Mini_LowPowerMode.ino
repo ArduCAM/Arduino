aac6017aba4bf67c0d0bd8a374ce1aa9212fd47c
Lee <admin@arducam.com>
2018-08-07 13:36:47 +0800

    Update ArduCAM_Mini_LowPowerMode.ino

--- ArduCAM/examples/mini/ArduCAM_Mini_LowPowerMode/ArduCAM_Mini_LowPowerMode.ino
+++ ArduCAM/examples/mini/ArduCAM_Mini_LowPowerMode/ArduCAM_Mini_LowPowerMode.ino
@@ -42,8 +42,16 @@ uint8_t temp;
 Serial.println(F("ACK CMD ArduCAM Start! END")); 
 // set the SPI_CS as an output:
 pinMode(SPI_CS, OUTPUT);
+digitalWrite(SPI_CS, HIGH);
 // initialize SPI:
 SPI.begin(); 
+  
+//Reset the CPLD
+myCAM.write_reg(0x07, 0x80);
+delay(100);
+myCAM.write_reg(0x07, 0x00);
+delay(100);
+  
 while(1){
   //Check if the ArduCAM SPI bus is OK
   myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
