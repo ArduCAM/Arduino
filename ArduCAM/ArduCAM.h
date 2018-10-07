10e92d66e85362782a009ace3cb8d345ac782e1d
Lee <admin@arducam.com>
2018-10-07 19:06:34 +0800

    Update ArduCAM.h

--- ArduCAM/ArduCAM.h
+++ ArduCAM/ArduCAM.h
@@ -232,6 +232,24 @@
 	#define F(X) (X)
 #endif
 
+#if defined(ARDUINO_ARCH_NRF52)
+    #define cbi(reg, bitmask) digitalWrite(bitmask, LOW)
+	#define sbi(reg, bitmask) digitalWrite(bitmask, HIGH)
+	#define pulse_high(reg, bitmask) sbi(reg, bitmask); cbi(reg, bitmask);
+	#define pulse_low(reg, bitmask) cbi(reg, bitmask); sbi(reg, bitmask);
+	
+	#define cport(port, data) port &= data
+	#define sport(port, data) port |= data
+	
+	#define swap(type, i, j) {type t = i; i = j; j = t;}
+	
+	#define fontbyte(x) cfont.font[x]  
+	
+	#define regtype volatile uint32_t
+	#define regsize uint32_t
+
+#endif
+
 
 /****************************************************/
 /* Sensor related definition 												*/
